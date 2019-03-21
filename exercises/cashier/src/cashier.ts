828a97aa3547c54d94c38c125261f2ce157fd923
Michael Bunsen <notbot@gmail.com>
2019-03-21 14:23:48 -0700

    chore: solution for cashier problem

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,35 @@
-// export function cashier() {
-//   return {};
-// }
+interface Item {
+    name: string,
+    price: number,
+    qty: number,
+}
+
+export function cashier() {
+    return {
+        length: 0,
+        total: 0,
+        add(name: string, 
+            price: number, 
+            qty ?: number) { 
+                qty = qty || 1;
+                this.length += qty;
+                this.total += price * qty;
+                return this;
+            },
+        addItem(item: Item) {
+            return this.add(
+                item.name, 
+                item.price, 
+                item.qty
+            );
+        }
+    }
+}
+
+let cart = cashier();
+
+cart
+  .add('Apple', 0.99)
+  .add('Pear', 0.99, 2)
+  .addItem({name:'Hand Cream', price:18, qty:1})
+;
